563f74d800e6e0befc13a21ebd11ad5ae8007890
Ocknon <>
2024-08-10 18:35:25 -0700

    Initialize project

--- src/main/java/com/camerasway/CameraSwayConfig.java
+++ src/main/java/com/camerasway/CameraSwayConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.camerasway;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface CameraSwayConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/camerasway/CameraSwayTest.java
+++ src/test/java/com/camerasway/CameraSwayTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.camerasway;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class CameraSwayTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(CameraSwayPlugin.class);
 		RuneLite.main(args);
 	}
 }
